f59f627fad4720afb34244ba222b5e776ea0168b
Paraschiv Andrei-Dumitru <pandreyy2014@gmail.com>
2020-06-12 20:15:32 +0300

    - Project Settings
    - Fix Translate
    - Replaced demo clip

--- Assets/Scripts/Language.cs
+++ Assets/Scripts/Language.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Language : MonoBehaviour{
+public class Language {
         public string chooseLang;
         public string prefLang(string nameLang){
                 switch(nameLang){
